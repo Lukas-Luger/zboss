55fdb1d703ba7d5f35406da0cc07d1f51b6a4f77
Lukas-Luger <lukas.luger@mailbox.tu-dresden.de>
2025-05-05 10:23:49 +0200

    check if nbh is already present (via parent annce)

--- nwk/nwk_join.c
+++ nwk/nwk_join.c
@@ -687,8 +687,9 @@ void zb_nlme_rejoin_scan_confirm(zb_uint8_t param) ZB_CALLBACK
             /* we might want to check if the base neighbor already exists to avoid
              * wrong default information (i.e. via dev annce)
              */
-            zb_nwk_neighbor_ext_to_base_tmp(best_parent);
-
+            if (!ZB_NWK_NEIGHBOR_GET_EXISTING(addr_ref)) {
+                zb_nwk_neighbor_ext_to_base_tmp(best_parent);
+            }
             /* join response timer will be started inside confirm callback */
         }
     }
